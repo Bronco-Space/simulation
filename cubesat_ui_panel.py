f71a9e722508f134d8f1028aa8d3013a1104676b
kkwik <kevinkwik6@gmail.com>
2021-02-07 15:21:58 -0800

    added initial ui panel

---
+++ cubesat_ui_panel.py
@@ -0,0 +1,41 @@
+import bpy
+
+
+class VIEW3D_PT_cubesat(bpy.types.Panel):
+    """Creates a Panel in the scene context of the properties editor"""
+    bl_label = "CubeSat Sim Controls" #Visual full name of menu
+    bl_idname = "SCENE_PT_layout" #Name of menu on the backend
+    bl_space_type = "VIEW_3D"   #Viewable ONLY in the 3d view port
+    bl_region_type = "UI" #Specifies that it appears in the tools bar
+    bl_category = "CubeSat" #Name on the side bar of the tools bar
+    bl_context = "objectmode" #Viewable ONLY in object mode
+
+    def draw(self, context):
+        layout = self.layout
+
+        scene = context.scene
+
+        # Create an row where the buttons are aligned to each other.
+        layout.label(text="Frames:")
+
+        row = layout.row(align=True)
+        row.prop(scene, "frame_start")
+        row.prop(scene, "frame_end")
+
+        # Second column, aligned
+        col = layout.column(align=True)
+        col.prop(context.active_object, "rotation_euler", text="Euler Angle")
+
+
+
+#Stuff that is required for the script to work
+def register():
+    bpy.utils.register_class(VIEW3D_PT_cubesat)
+
+
+def unregister():
+    bpy.utils.unregister_class(VIEW3D_PT_cubesat)
+
+
+if __name__ == "__main__":
+    register()
