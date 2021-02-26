4e20f85b53cec9e6331b20082e53bb09411d4139
Ryan <78242399+notBajaBlast@users.noreply.github.com>
2021-02-25 22:00:46 -0800

    Scaled Earth to 1/10,000

--- gravity_calc.py
+++ gravity_calc.py
@@ -5,12 +5,12 @@ from math import sqrt
 
 def gravForce():
     
-    cube = bpy.data.objects["cubesat"].location
+    cube = bpy.data.objects["cubesat"].location * 10000
     cubeR = sqrt((cube[0]**2) + (cube[1]**2) + (cube[2]**2)) #in m
     massC = 1.75 # in kg
     
     
-    earthDim = bpy.data.objects["EarthSurface"].dimensions
+    earthDim = bpy.data.objects["EarthSurface"].dimensions * 10000
     earthR = earthDim[1]/2
     dens = 5515 #kg/m^3
     volE = (4/3)*(3.14159)* (earthR)**3 #m^3
@@ -20,18 +20,18 @@ def gravForce():
     gravC = (6.67408 * 10**(-11))
     unitVectR = cube / cubeR
     
-    force = -((gravC * massE *massC) / (cubeR**2)) * unitVectR * (10**6) #micronewtons
+    force = -((gravC * massE *massC) / (cubeR**2)) * unitVectR  #newtons
     
     return force
 
 #gravitational acceleration experienced by satellite
 def gravAccel():
     
-    cube = bpy.data.objects["cubesat"].location
+    cube = bpy.data.objects["cubesat"].location * 10000
     cubeR = sqrt((cube[0]**2) + (cube[1]**2) + (cube[2]**2)) #in m
     
 
-    earthDim = bpy.data.objects["EarthSurface"].dimensions
+    earthDim = bpy.data.objects["EarthSurface"].dimensions * 10000
     earthR = earthDim[1]/2
     dens = 5515 #kg/m^3
     volE = (4/3)*(3.14159)* (earthR)**3 #m^3
@@ -39,5 +39,5 @@ def gravAccel():
     gravE = ((6.67408 * 10**(-11)) * massE) / (earthR**2)
     
     unitVectR = cube / cubeR
-    accel = -(gravE * (earthR**2) / (cubeR ** 2))* unitVectR * 10**6 #micrometers/s^2
+    accel = -(gravE * (earthR**2) / (cubeR ** 2))* unitVectR #meters/s^2
     return accel
