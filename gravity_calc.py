ac0ac4e2cbcde28a22d091c59a5075de45e76a9c
Ryan <78242399+notBajaBlast@users.noreply.github.com>
2021-05-29 21:37:47 -0700

    Update gravity_calc.py

--- gravity_calc.py
+++ gravity_calc.py
@@ -34,70 +34,19 @@ def gravAccel():
     
 
     earthDim = bpy.data.objects["EarthSurface"].dimensions * 10000
-    earthR = earthDim[1]/2
+    earthR = sqrt((earthDim[0]**2) + (earthDim[1]**2) + (earthDim[2]**2))
     dens = 5515 #kg/m^3
     volE = (4/3)*(3.14159)* (earthR)**3 #m^3
     massE = dens * volE #kg
-    gravE = ((6.67408 * 10**(-11)) * massE) / (earthR**2)
-    
+
+    unitvectE = (earthDim / 2) / earthR
     unitVectR = cube / cubeR
+
+    gravE = ((6.67408 * 10**(-11)) * massE) / (earthR**2) * unitvectE
+    
+    
     accel = -(gravE * (earthR**2) / (cubeR ** 2))* unitVectR #meters/s^2
     return accel
-
-def angVel(times, angles):
-    #values need to be cycled in and out of these two arrays of arrays based off of a time step. this will probably be done with a series of if else function
-    rotations = Rotation.from_euler('XYZ', angles, degrees=True)
-    spline = RotationSpline(times, rotations)
-    angular_vel = np.rad2deg(spline(times, 1))
-    return angular_vel
-    
-
-
-
-cubePos = bpy.data.objects["cubesat"].rotation_euler
-times = [0] #example data without timestep
-rotPos = [[cubePos[0], cubePos[1], cubePos[2]]] #rotation positions
-dispC = 1
-
-def arManager():
-    cubePos = bpy.data.objects["cubesat"].rotation_euler
-    global times
-    global rotPos
-    global dispC
-    if (len(times)) < 15: #lists need to dynamically increase in size otherwise the readout from the angular velocity function are incorrect
-        times.append(dispC)
-        rotPos.append(([cubePos[0], cubePos[1], cubePos[2]]))
-        dispC = dispC + 1
-    else:
-        times.append(dispC) #test value
-        times.pop(0)
-        rotPos.append(([cubePos[0], cubePos[1], cubePos[2]]))
-        rotPos.pop(0)
-        dispC = dispC + 1
-        
-    print(times)
-    print(rotPos)
-    print(dispC)
-    
-    
-#x = True
-#while x == True:
- #   t = Timer(2, arManager)
- #   testRotate = bpy.data.objects["cubesat"]
-  #  testRotate.rotation_euler = [dispC, dispC, dispcC]
-  #  t.start()
-   # t.join()
-    #if len(times) == 10:
-     #   t.cancel()
-     #   print("test over")
-      #  x = False 
-        
-        
-    
-    
-
-
-    
 
 
 
