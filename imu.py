8a2a3cc7f7702cf020308222388d93d7e8f3ca44
SirSam <sbennett@cpp.edu>
2021-07-14 04:15:59 -0700

    prototype gyro

--- imu.py
+++ imu.py
@@ -6,17 +6,20 @@ import mathutils as mat
 class IMU:
 
     def __init__(self):
-        self._satQuat = bpy.data.objects["cubesat"].rotation_quaternion     # initial value needed for qCurrent
-        self._qCurrent = Quaternion(self._satQuat[0], self._satQuat[1], self._satQuat[2], self._satQuat[3]).normalised
-        self._angVarQuat = Quaternion(1,0,0,0)
+        # self._satQuat = bpy.data.objects["cubesat"].rotation_quaternion     # initial value needed for qCurrent
+        # self._qCurrent = Quaternion(self._satQuat[0], self._satQuat[1], self._satQuat[2], self._satQuat[3]).normalised
+        # self._angVarQuat = Quaternion(1,0,0,0)
         self.magnetic = ()
         self.gyro = ()
         self.acceleration = ()
 
-    def UpdateQuat(self, newQuat):
+    def UpdateGyro(self, newQuat):
         if type(newQuat) == type(self._qCurrent):
-            self._angVarQuat = newQuat
+            self.gyro = tuple(newQuat.get_axis())
         else:
-            print("IMU.UpdateQuat ERROR: parameter not of Quaternion type.")
+            print("IMU.UpdateGyro ERROR: parameter not of Quaternion type.")
+
+    def UpdateAcc(self):
+        self.acceleration = tuple(bpy.data.objects["cubesat"])
 
 test_imu = IMU()
