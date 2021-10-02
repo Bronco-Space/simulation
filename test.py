b0ad555f4957f6595339f774eb4d9e50b032f6c4
Ryan <78242399+notBajaBlast@users.noreply.github.com>
2021-10-01 23:10:59 -0700

    added parameters to test.py

--- test.py
+++ test.py
@@ -10,6 +10,6 @@ while x < 1:
     
     magf = mag.get_magnetic_force(bpy.data.objects['cubesat'].location)
     torque = magT.calcTorque(magf)
-    magT.controlSys(torque, aVel.qCurrent)
-    
+    omega = magT.controlSys(torque, aVel.qCurrent)
+    aVel.setAngVar(omega[0], omega[1], omega[2])
     x = x + 1
